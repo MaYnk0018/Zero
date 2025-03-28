388387d7eef8f42d506b4a5d2e3ad2d707cf7798
MaYnK0018 <ms323939@gmail.com>
2025-03-28 22:42:36 +0530

    css modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { QueryProvider } from './context/queryContext';
 import QueryEditor from './components/queryEditor';
 import QuerySelector from './components/querySelector';
 import ResultTable from './components/resultTable';
-
+import './index.css';
 
 const App: React.FC = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+  const sidebarWidth = showSidebar ? 250 : 50;
+
   return (
     <QueryProvider>
       <div className="app-container">
-        <div className="sidebar">
-          <QuerySelector />
-        </div>
-        <div className="main-content">
-          <div className="right-top">
-            <QueryEditor />
+        <div className="content-container">
+          <div
+            className="sidebar-container"
+            style={{ 
+              width: `${sidebarWidth}px`, 
+              minWidth: `${sidebarWidth}px`, 
+              flexShrink: 0 
+            }}
+          >
+            {showSidebar && <QuerySelector />}
+            <button
+              className="toggle-sidebar-button"
+              onClick={() => setShowSidebar(prev => !prev)}
+            >
+              {showSidebar ? '<' : '>'}
+            </button>
           </div>
-          <div className="right-bottom">
-            <ResultTable />
+          <div
+            className="main-content"
+            style={{ 
+              flex: 1, 
+              overflow: 'hidden'  
+            }}
+          >
+            <div className="right-top">
+              <QueryEditor />
+            </div>
+            <div className="right-bottom">
+              <ResultTable />
+            </div>
           </div>
         </div>
       </div>
